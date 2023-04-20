6b46a421c31ca9f6beb01b0891560b41b6516faf
Ân Trần Bảo <20520377@ms.uit.edu.vn>
2023-04-20 14:20:30 +0700

    feat: add i18n and 2 pages

---
+++ pages/tin-tuc/index.jsx
@@ -0,0 +1,51 @@
+import { NextSeo } from "next-seo";
+import useTranslation from "next-translate/useTranslation";
+import Post from "~/components/Post";
+import PostServices from "~/services/PostServices";
+import { STATUS_SUCCESS } from "~/services/urlAPI";
+import PageHeading from "~/ui-components/PageHeading";
+import { convertImageNameToImageUrl } from "~/utils/image-processing";
+
+export const getServerSideProps = async () => {
+  let posts = [];
+
+  try {
+    const { status, data } = await PostServices.getPosts("NEWS");
+    if (status === STATUS_SUCCESS) {
+      posts = data.map((post) => ({
+        ...post,
+        thumbnail: convertImageNameToImageUrl(post.thumbnail),
+      }));
+    }
+  } catch (err) {}
+
+  return {
+    props: {
+      posts: posts,
+    },
+  };
+};
+
+export default function NewsPost({ posts }) {
+  const { t } = useTranslation();
+
+  return (
+    <div className="flex-1">
+      <NextSeo title={t("common:tin-tuc")} description={t("common:tin-tuc")} />
+      <div>
+        <PageHeading content={t("common:tin-tuc")} />
+        <div className="container mx-auto py-12 px-4 flex flex-col gap-12">
+          {posts.length > 0 ? (
+            posts.map((post) => <Post key={post.id} post={post} />)
+          ) : (
+            <div className="w-full h-full min-h-[100px] flex flex-col items-center justify-center">
+              <p className="text-lg font-semibold text-primary">
+                {t("post:khong-co-bai-viet")}
+              </p>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
